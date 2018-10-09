79c084e81a4e631cdb2be6584c16f19986ec6665
stdemicheli <stdemicheli@gmail.com>
2018-10-09 21:38:58 +0200

    First commit.

---
+++ ios-astronomy-objc/ios-astronomy-objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-astronomy-objc
+//
+//  Created by De MicheliStefano on 09.10.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-astronomy-objc/ios-astronomy-objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-astronomy-objc
+//
+//  Created by De MicheliStefano on 09.10.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
